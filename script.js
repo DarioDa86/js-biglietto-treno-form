4a64dbd86dd5c499af6f26cce2995640f595e6e3
DarioDa86 <dario.dagnelli@gmail.com>
2021-09-01 09:26:50 +0200

    Fixato codice, funziona. Css da fixare

--- script.js
+++ script.js
@@ -7,26 +7,47 @@
 // Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo). 
 
 // 1- dichiaro le variabili dei pulsanti (elementi attivi della pagina)
-var biglietto = document.getElementById("crea-biglietto");
+var creaBiglietto = document.getElementById("crea-biglietto");
 var reset = document.getElementById("reset-dati");
 
 // 2.1 specifico le funzioni del bottone "biglietto" e la sua funzione con i campi da compilare (input)
-biglietto.addEventListener("click",
+creaBiglietto.addEventListener("click",
     function(){
 
+        // 2.a - Prendo i dati inseriti dall'utente
         var nome = document.getElementById("nome").value;
         var km = parseInt(document.getElementById("km").value);
-        var anni = document.getElementById("anni").value;
+        var anni = parseInt(document.getElementById("anni").value);
+
+        // 2.b - Applico le fasce di prezzo ed eventuali sconti
+        var prezzo = km * 0.21;
+        var offerta = "biglietto standard";
 
         if ( anni == "minorenne") {
-
+            // sconto 20%
+            prezzo = prezzo - (prezzo * 0.20);
+            offerta = "Sconto minorenne";
         } else if ( anni == "over") {
+            // sconto 40%
+            prezzo = prezzo - (prezzo * 0.40);
+            offerta = "Sconto Silver";
+        } 
 
-        } else {
+        // Arrotondare i 2 decimali del prezzo
+        prezzo = prezzo.toFixed(2);
 
-        }
+        document.getElementById("dati-passeggero").innerHTML = nome;
+
+        document.getElementById("tipo-offerta").innerHTML = offerta;
+
+        document.getElementById("costo-biglietto").innerHTML = prezzo + " €";
+
+        document.getElementById("num-carrozza").innerHTML = Math.floor(Math.random() * 9) + 1;
+
+        document.getElementById("codice-cp").innerHTML = Math.floor(Math.random() * (99999 - 90000) ) + 90000;
 
         document.getElementById("totale-biglietto").classList.add("mostra");
+
     }
 );
 
@@ -34,9 +55,11 @@ biglietto.addEventListener("click",
 
 reset.addEventListener("click",
     function(){
-        document.getElementById("nome").value = " ";
-        document.getElementById("km").value = " ";
-        document.getElementById("anni").value = " ";
+        document.getElementById("totale-biglietto").classList.remove("mostra");
+
+        document.getElementById("nome").value = "";
+        document.getElementById("km").value = "";
+        document.getElementById("anni").value = "0";
     }
 
 );
